589667068325516f641972a455760ef883da9c0d
theokai <44306219+theokai@users.noreply.github.com>
2018-11-30 15:22:42 -0800

    Update ReminderActivity.kt

--- app/src/main/java/edu/ucsc/retap/retap/reminder/ReminderActivity.kt
+++ app/src/main/java/edu/ucsc/retap/retap/reminder/ReminderActivity.kt
@@ -1,8 +1,11 @@
 package edu.ucsc.retap.retap.reminder
 
+import android.content.DialogInterface
+import android.support.v7.app.AlertDialog
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.EditText
 import edu.ucsc.retap.retap.R
 import edu.ucsc.retap.retap.common.BaseActivity
 
@@ -19,8 +22,20 @@ class ReminderActivity : BaseActivity() {
     override fun onOptionsItemSelected(item: MenuItem):Boolean {
         return when (item.itemId) {
             R.id.action_add_task -> {
-                Log.d(TAG, "Add a new task")
-                true
+                val taskEditText = EditText(this)
+                val dialog = AlertDialog.Builder(this)
+                    .setTitle("Add a Reminder")
+                    .setMessage("Keep It Short")
+                    .setView(taskEditText)
+                    .setPositiveButton("Add", object: DialogInterface.OnClickListener() {
+                        fun onClick(dialog:DialogInterface, which:Int) {
+                            val task = String.valueOf(taskEditText.getText())
+                            Log.d(TAG, "Task to add: " + task)
+                        }
+                    })
+                    .setNegativeButton("Cancel", null)
+                    .create()
+                dialog.show()
             }
             else -> super.onOptionsItemSelected(item)
         }
